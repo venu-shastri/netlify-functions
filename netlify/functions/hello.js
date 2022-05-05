a1a473d0304b9c75cdd8cda0981c348ba18d5ea4
Venugopal K <venu.shastri@gmail.com>
2022-05-05 11:41:45 +0530

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,5 +1,18 @@
+var faunadb = require('faunadb'),
+  q = faunadb.query
 exports.handler=async function(event,context){
 
+    //fauna code 
+    var client = new faunadb.Client({ secret: process.env.STOCKTRADER_FAUNADB_SECRET })
+    
+    var createP = client.query(
+                q.Create(q.Collection('users'), { data: { userName: 'tom@gmail.com' ,roleName:'Analyst' })
+      );
+        
+     createP.then(function(response) {
+                console.log(response.ref) // Would log the ref to console.
+        })
+    
     return{
         statusCode:200,
         body:JSON.stringify({messge:"HelloWorld"})
